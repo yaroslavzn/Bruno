41399ee1cac093d6dae2372ffe21e7f15eacac77
Yaroslav Mishchenko <myv@Yaroslavs-iMac.local>
2017-07-15 19:41:15 +0300

    finished gallery section desktop design

--- app/js/common.js
+++ app/js/common.js
@@ -1,9 +1,18 @@
 $(document).ready(function () {
+    // Header Slider
     $('.header__slide-list').slick({
         dots: true,
         infinite: false
     });
+
+    // Section Team Slider
     $('.team__slider').slick({
         infinite: false
+    });
+
+    // Section Gallery Slider
+    $('.gallery__list').slick({
+        infinite: false,
+        slidesToShow: 3
     });
 });
